5a8c5204162102d0d3f7d91e3591b6d3bc5a22e1
Ermal Kelmendi <116918975+Ermali1@users.noreply.github.com>
2024-03-28 02:40:22 +0100

    Bootstrap.
    Bootstrap install.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Greating from './components/shared/greeting/Greeting';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import About from './pages/About/About';
 import NavBar from './components/shared/NavBar/NavBar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
